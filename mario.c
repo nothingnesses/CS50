0b8d995dce94f727111a9dde53b2aef71c36c494
VoidNoire <VoidNoire@users.noreply.github.com>
2020-07-12 20:26:36 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -16,14 +16,16 @@ typedef struct Characters {
   const char character;
   const int amount;
   char *output;
+  const bool is_malloced;
 } Characters;
 
 // Returns a `Characters` instance
-Characters characters_instance(char character, int amount, char *output) {
+Characters characters_instance(const char character, const int amount, char *output, const bool is_malloced) {
   return (Characters) {
     character,
     amount,
-    output
+    output,
+    is_malloced
   };
 }
 
@@ -38,8 +40,10 @@ Characters evaluate_characters(Characters input) {
     } else {
       output_buffer[0] = '\0';
       snprintf(output_buffer, output_length, "%s%c", input.output, input.character);
-      const Characters output = evaluate_characters(characters_instance(input.character, input.amount - 1, output_buffer));
-      free(output_buffer);
+      const Characters output = evaluate_characters(characters_instance(input.character, input.amount - 1, output_buffer, true));
+      if (input.is_malloced) {
+        free(input.output);
+      }
       return output;
     }
   } else {
@@ -52,14 +56,27 @@ typedef struct Pyramids {
   const int height;
   const int accumulator;
   char *output;
+  const bool is_malloced;
+  const Characters left_padding_buffer;
+  const Characters pounds_buffer;
 } Pyramids;
 
 // Returns a `Pyramids` instance
-Pyramids pyramid_instance(Pyramids *input, int accumulator, char *output) {
+Pyramids pyramid_instance(
+  Pyramids *input,
+  int accumulator,
+  char *output,
+  bool is_malloced,
+  Characters left_padding_buffer,
+  Characters pounds_buffer
+  ) {
   return (Pyramids) {
     input->height,
     accumulator,
-    output
+    output,
+    is_malloced,
+    left_padding_buffer,
+    pounds_buffer
   };
 }
 
@@ -67,8 +84,10 @@ Pyramids pyramid_instance(Pyramids *input, int accumulator, char *output) {
 Pyramids pyramid_evaluate(Pyramids input) {
   if (input.accumulator > 0) {
     const int decremented = input.accumulator - 1;
-    const char *left_padding = evaluate_characters(characters_instance(' ', decremented, "")).output;
-    const char *pounds = evaluate_characters(characters_instance('#', input.height - decremented, "")).output;
+    const Characters left_padding_buffer = evaluate_characters(characters_instance(' ', decremented, "", false));
+    const char *left_padding = left_padding_buffer.output;
+    const Characters pounds_buffer = evaluate_characters(characters_instance('#', input.height - decremented, "", false));
+    const char *pounds = pounds_buffer.output;
     const int output_length = snprintf(
       NULL,
       0,
@@ -93,20 +112,41 @@ Pyramids pyramid_evaluate(Pyramids input) {
         pounds,
         pounds
       );
-      const Pyramids output = pyramid_evaluate(pyramid_instance(&input, decremented, output_buffer));
-      free(output_buffer);
+      const Pyramids output = pyramid_evaluate(pyramid_instance(&input, decremented, output_buffer, true, left_padding_buffer, pounds_buffer));
+      if (input.left_padding_buffer.is_malloced) {
+        free(input.left_padding_buffer.output);
+      }
+      if (input.pounds_buffer.is_malloced) {
+        free(input.pounds_buffer.output);
+      }
+      if (input.is_malloced) {
+        free(input.output);
+      }
       return output;
     }
+  }
+  if (input.left_padding_buffer.is_malloced) {
+    free(input.left_padding_buffer.output);
+  }
+  if (input.pounds_buffer.is_malloced) {
+    free(input.pounds_buffer.output);
   }
   return input;
 }
 
 int main(void) {
   const int height = get_height(get_int("Height: "));
-  printf("%s", pyramid_evaluate((Pyramids) {
+  Pyramids pyramids = pyramid_evaluate((Pyramids) {
     height,
     height,
-    ""
-  }).output);
+    "",
+    false,
+    // Dummy `Characters` instance
+    characters_instance(' ', 0, "", false),
+    // Dummy `Characters` instance
+    characters_instance(' ', 0, "", false)
+  });
+  printf("%s", pyramids.output);
+  free(pyramids.output);
   return 0;
 }
