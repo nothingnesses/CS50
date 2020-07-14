4532291c10fa6a2fcea6ae551e40658d21897b8d
VoidNoire <VoidNoire@users.noreply.github.com>
2020-07-14 21:41:46 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -68,7 +68,7 @@ bool vote(string name)
 {
     for (int candidates_index = 0; candidates_index < MAX; ++candidates_index)
     {
-        if (strcmp(candidates[candidates_index].name, name)) {
+        if (strcmp(candidates[candidates_index].name, name) == 0) {
             ++candidates[candidates_index].votes;
             return true;
         }
