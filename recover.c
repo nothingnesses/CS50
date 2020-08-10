db9346d9d1a744ccc4edbbb9211c41c09ca1d471
VoidNoire <VoidNoire@users.noreply.github.com>
2020-08-10 19:38:00 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <errno.h>
 
-bool is_jpeg_start(char buffer_input[]) {
+bool is_jpg_start(char buffer_input[]) {
   return (buffer_input[0] == '\xFF' &&
           buffer_input[1] == '\xD8' &&
           buffer_input[2] == '\xFF' &&
@@ -22,20 +22,20 @@ int main(int argc, char *argv[]) {
   }
   char buffer[512];
   int output_counter = 0;
-  bool in_jpeg = false;
-  // "000.jpeg\0"
-  char output_name[9];
+  bool in_jpg = false;
+  // "000.jpg\0"
+  char output_name[8];
   FILE *output_file_pointer = NULL;
   // iterate through input file
   for (;;) {
     if (fread(buffer, sizeof(buffer), 1, image_file_pointer) >= 1) {
       // have read 512 bytes, might not be end of file
-      if (in_jpeg) {
-        if (is_jpeg_start(buffer)) {
-          // in jpeg, in start of jpeg = new jpeg file
+      if (in_jpg) {
+        if (is_jpg_start(buffer)) {
+          // in jpg, in start of jpg = new jpg file
           fclose(output_file_pointer);
           output_name[0] = '\0';
-          snprintf(output_name, sizeof(output_name), "%03d.jpeg", ++output_counter);
+          snprintf(output_name, sizeof(output_name), "%03d.jpg", ++output_counter);
           output_file_pointer = fopen(output_name, "a");
           if (output_file_pointer == NULL) {
             printf("ERROR: %d. `fopen` failed to open output file \"%s\" for appending.\n", errno, output_name);
@@ -43,14 +43,14 @@ int main(int argc, char *argv[]) {
           }
           fwrite(buffer, sizeof(buffer), 1, output_file_pointer);
         } else {
-          // in jpeg, not in start of jpeg
+          // in jpg, not in start of jpg
           fwrite(buffer, sizeof(buffer), 1, output_file_pointer);
         }
-      } else if (is_jpeg_start(buffer)) {
-        // not in jpeg, in start of jpeg
-        in_jpeg = true;
+      } else if (is_jpg_start(buffer)) {
+        // not in jpg, in start of jpg
+        in_jpg = true;
         output_name[0] = '\0';
-        snprintf(output_name, sizeof(output_name), "%03d.jpeg", output_counter);
+        snprintf(output_name, sizeof(output_name), "%03d.jpg", output_counter);
         output_file_pointer = fopen(output_name, "a");
         if (output_file_pointer == NULL) {
           printf("ERROR: %d. `fopen` failed to open output file \"%s\" for appending.\n", errno, output_name);
